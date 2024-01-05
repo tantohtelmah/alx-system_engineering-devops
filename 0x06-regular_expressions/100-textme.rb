4b1aed662716d3c578a390e38ac82d8adbd305e2
tantohtelmah <tantohtelmah@gmail.com>
2024-01-04 20:46:51 -0500

    regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,6 +3,8 @@
 # Extract the text after "from"
 text = ARGV[0]
 match = /(?<=from:)[^\]]+/.match(text)
+match1 = /(?<=to:)[^\]]+/.match(text)
+match2 = /(?<=flags:)[^\]]+/.match(text)
 
 # Print the extracted text
-puts match[0]
+puts match[0], match1[0], match2[0]
