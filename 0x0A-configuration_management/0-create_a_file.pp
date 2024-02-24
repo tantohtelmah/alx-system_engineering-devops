f545168ffcfcdbc132c2e22ac81c90a939ae3056
tantohtelmah <tantohtelmah@gmail.com>
2024-02-23 21:22:48 -0500

    tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creating a file in /tmp.
 
-$file = { '/tmp/school':
-  ensure   =>  'file',
-  mode     =>  '0744',
-  owner    =>  'www-data',
-  group    =>  'www-data',
-  content  =>  'I love Puppet',
+file { '/tmp/school':
+  ensure  =>  'file',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet',
 }
