28b0b2f3407192a35fb6ec2900b9a5783d9d86cd
tantohtelmah <tantohtelmah@gmail.com>
2024-02-27 23:57:59 -0500

    moves

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Create a manifest to kill the process named "killmenow"
-exec { 'kill_killmenow_process':
-  command => 'pkill -9 killmenow',
-  path    => ['/usr/bin', '/bin'],  # Specify the path to pkill
-  onlyif  => 'pgrep killmenow',     # Only execute if the process is running
-}
--- 0x0A-configuration_management/README.md
+++
@@ -1,2 +0,0 @@
-Puppet is a powerful tool used for continuous configuration automation
-n
